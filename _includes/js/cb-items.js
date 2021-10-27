b9d7846dd39ef243fa9c6425a617e8919ed59f80
evanwill <evanpeterw@gmail.com>
2021-10-27 10:52:57 -0700

    move cb-items init to include

---
+++ _includes/js/cb-items.js
@@ -0,0 +1,22 @@
+/* Load items from Sheets, store in sessionStorage, or load from sessionStorage */
+var cb_items = [];
+var query = "";
+// function to process items from Sheets and store
+function cb_items_init(results) {
+  cb_items = results.data;
+  sessionStorage.setItem("cb_items_store", JSON.stringify(cb_items));
+  pageInit(cb_items);
+}
+// check for sessionStored items
+if (sessionStorage.getItem("cb_items_store")) {
+  cb_items = JSON.parse(sessionStorage.getItem("cb_items_store"));
+  pageInit(cb_items);
+
+} else { 
+  /* use papaparse to get metadata from google sheets, then init page */
+  Papa.parse("{{ site.metadata-csv-link }}", {
+    download: true,
+    header: true,
+    complete: (results) => cb_items_init(results)
+  });
+}
