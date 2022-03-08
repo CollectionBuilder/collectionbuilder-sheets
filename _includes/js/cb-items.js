efccbfe3601219201a232e1ed47f54977ae49e16
evanwill <evanpeterw@gmail.com>
2022-03-08 13:57:40 -0800

    update to item js

--- _includes/js/cb-items.js
+++ _includes/js/cb-items.js
@@ -3,18 +3,22 @@ var cb_items = [];
 var config_metadata = "{{ site.metadata-csv | relative_url }}";
 
 // function to process items from Sheets and store
-function cb_items_init(config_metadata) {
+function cb_items_init(metadata_url) {
   /* use papaparse to get metadata from configured CSV URL, then init page */
-  Papa.parse(config_metadata, {
+  Papa.parse(metadata_url, {
     download: true,
     header: true,
     complete: (results) => { 
       cb_items = results.data.filter(item => item["objectid"]);
       sessionStorage.setItem("cb_items_store", JSON.stringify(cb_items));
       pageInit(cb_items);
+    },
+    error: (err) => {
+      alert("There is an error parsing your CSV! Please check the configured URL or file. The most common issue is incorrect filename, path, or URL so that your CSV is not found.");
     }
   });
 }
+
 // check for sessionStored items
 if (sessionStorage.getItem("cb_items_store")) {
   cb_items = JSON.parse(sessionStorage.getItem("cb_items_store"));
@@ -23,41 +27,26 @@ if (sessionStorage.getItem("cb_items_store")) {
 } else if (config_metadata != "") { 
   cb_items_init(config_metadata);
 } else {
-  // if no CSV is configured, ask to load local file
-  // set up select file form
-  var fileSelectCard = `<div class="w-50 mx-auto my-5">
-      <div class="card p-5" id="fileSelectCard">
-        <div class="card-body">
-          <h2>Select Metadata!</h2>
-          <p>No metadata CSV is configured in this project's "_config.yml". If you would like to test a CSV, select the file using the form below</p>
-          <div class="custom-file">
-            <input type="file" accept=".csv" class="custom-file-input" id="selectFile">
-            <label class="custom-file-label" for="selectFile">Choose metadata CSV file</label>
-          </div>
-        </div>
-      </div>
-    </div>`;
-  document.querySelector("main").innerHTML = fileSelectCard;
-  // add listener to file selector
-  var fileSelector = document.getElementById("selectFile");
-  fileSelector.addEventListener('change', (event) => {
-    // give file to papa parse and init
-    var selectedFile = event.target.files[0];
-    cb_items_init(selectedFile);
-    // reload
-    location.reload();
-  });
+  // if no CSV is configured, redirect to the set up page
+  window.location.href = "{{ '/setup/' | absolute_url }}";
 
 }
+
 {% if site.development-refresh == true %}
-var refreshButton = document.createElement("div");
-refreshButton.classList.add("fixed-top");
-refreshButton.innerHTML = '<button class="btn btn-sm btn-secondary m-3" onclick="resetStore()" id="refreshButton">Refresh Metadata</button>';
-document.body.appendChild(refreshButton);
-function resetStore () {
-  // remove data
-  sessionStorage.removeItem("cb_items_store");
-  // reload
-  location.reload();
+if (sessionStorage.getItem("cb_metadata_set")) { 
+  var refreshButton = document.createElement("div");
+  refreshButton.classList.add("dev-buttons");
+  refreshButton.innerHTML = `<div class="btn-group-vertical">
+    <button class="btn btn-sm btn-secondary" onclick="resetStore()" id="refreshButton">Reset Metadata</button>
+    <a class="btn btn-sm btn-info" href="{{ '/setup/' | relative_url }}">Configure Metadata</a>
+    </div>`;
+  document.body.appendChild(refreshButton);
+  function resetStore () {
+    // remove data
+    sessionStorage.removeItem("cb_items_store");
+    sessionStorage.removeItem("cb_metadata_set");
+    // reload
+    location.reload();
+  }
 }
 {%- endif -%}
